a854b9a9eb51bd4ac03b732d943a198be9316f04
Leo Schweiger <schleoooooo@gmail.com>
2025-10-11 13:18:37 +0200

    small improvements

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -9,7 +9,6 @@ export const staticUrl = "https://leoschweiger.com";
 export const resume = "/resume-leo-schweiger.pdf"
 export const email = "leo.schweiger@icloud.com"
 export const emailDisplay = "leo.schweiger＠icloud.com"
-export const portrait = "/src/media/leo-schweiger-portrait.webp"
 export const links = {
     [emailDisplay]: `mailto:${email}`,
     "linked in": "https://www.linkedin.com/in/leo-schweiger-256987339/",
