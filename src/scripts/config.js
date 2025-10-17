72bbef347ae3e998e28d081bd5210b80032ddeea
Leo Schweiger <schleoooooo@gmail.com>
2025-10-18 01:14:19 +0200

    improvements to pdf gen

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,18 +1,27 @@
+export const staticUrl = "https://leoschweiger.com";
 export const aboutMeLong =
-    'I’m a student and game developer based in Salzburg, Austria, focused on creating effective, interesting and fun software. Since 2018, I have been working on projects ranging from utility tools to fully released games. My technical and artistic background allowed me to create multiple <a href="/games#developed">game projects</a> already.';
+    `I’m a student and game developer based in Salzburg, Austria, focused on creating effective, interesting and fun software. Since 2018, I have been working on projects ranging from utility tools to fully released games. My technical and artistic background allowed me to create multiple <a href="${staticUrl}/games#developed">game projects</a> already.`;
 export const aboutMeShort =
     'I’m a game developer focused on creating interesting and fun software. My technical and artistic background allowed me to create multiple games already.';
 
 export const mainTitle = "leo schweiger";
-export const staticUrl = "https://leoschweiger-com-git-static-content-leoschweigers-projects.vercel.app";
-// export const staticUrl_ = "https://leoschweiger.com";
 
 export const resume = "/resume-leo-schweiger.pdf"
 export const email = "leo.schweiger@icloud.com"
 export const emailDisplay = "leo.schweiger＠icloud.com"
 export const links = {
     [emailDisplay]: `mailto:${email}`,
+    [email]: `mailto:${email}`,
     "linked in": "https://www.linkedin.com/in/leo-schweiger-256987339/",
     "itch": "https://leoschweiger.itch.io/",
     "github": "https://github.com/leoschweiger",
+    "leoschweiger.com": staticUrl,
+    "resume": `${staticUrl}/resume-leo-schweiger.pdf`
+};
+
+export const footerLinks = {
+    [emailDisplay]: links[emailDisplay],
+    "linked in": links["linked in"],
+    "itch": links["itch"],
+    "github": links["github"]
 };
